636e2ab0795e3feb32bef57d5c69b230b5d647a6
Karbbone <clement.maillet56@gmail.com>
2025-07-17 23:11:37 +0200

    feat : import font

--- client/src/features/login/Login.view.vue
+++ client/src/features/login/Login.view.vue
@@ -1,31 +1,21 @@
+<script setup lang="ts">
+import Wrapper from "@/shared/components/Wrapper.vue";
+</script>
+
 <template>
-  <div>
+  <Wrapper>
     <h1>Login</h1>
-    <form @submit.prevent="handleSubmit">
+    <form>
       <div>
         <label for="email">Email:</label>
-        <input type="email" id="email" v-model="email" required />
+        <input type="email" id="email" required />
       </div>
       <div>
         <label for="password">Password:</label>
-        <input type="password" id="password" v-model="password" required />
+        <input type="password" id="password" required />
       </div>
       <button type="submit">Login</button>
     </form>
     <router-link to="/">Back to Home</router-link>
-  </div>
+  </Wrapper>
 </template>
-
-<script setup lang="ts">
-import { ref } from "vue";
-
-const email = ref("");
-const password = ref("");
-
-const handleSubmit = () => {
-  console.log("Login attempt:", {
-    email: email.value,
-    password: password.value,
-  });
-};
-</script>
