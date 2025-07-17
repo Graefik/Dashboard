b6df5d6230d058e16551cb45bef3031f327dc9b2
Karbbone <clement.maillet56@gmail.com>
2025-07-17 23:11:20 +0200

    feat : create wrapper component

---
+++ client/src/shared/components/Wrapper.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.wrapper {
+  height: 100%;
+  width: 100%;
+  padding: 0 20px;
+
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 1280px;
+}
+</style>
