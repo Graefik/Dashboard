7c0fa4f77f46d2fc23dfe323e0e20baa872ed607
Karbbone <clement.maillet56@gmail.com>
2025-07-24 13:31:51 +0200

    chore(front-charts) : init apexcharts for vue

--- client/src/features/dashboard/Dashboad.view.vue
+++ client/src/features/dashboard/Dashboad.view.vue
@@ -1,5 +1,90 @@
 <script setup lang="ts">
 import MainLayout from "@/shared/layouts/MainLayout.vue";
+import type { ApexOptions } from "apexcharts";
+import { ref } from "vue";
+
+const colors = [
+  "#008FFB",
+  "#00E396",
+  "#FEB019",
+  "#FF4560",
+  "#775DD0",
+  "#546E7A",
+  "#26a69a",
+  "#D10CE8",
+];
+
+interface SeriesData {
+  name?: string;
+  data: number[];
+}
+
+const series = ref<SeriesData[]>([
+  {
+    name: "Score",
+    data: [21, 22, 10, 28, 16, 21, 13, 30],
+  },
+]);
+
+const chartOptions = ref<ApexOptions>({
+  chart: {
+    height: 350,
+    type: "bar",
+    toolbar: {
+      show: false,
+    },
+    events: {
+      click: function (chart, w, e) {
+        console.log(chart, w, e);
+      },
+    },
+  },
+  colors: colors,
+  plotOptions: {
+    bar: {
+      columnWidth: "45%",
+      distributed: true,
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  legend: {
+    show: false,
+  },
+  tooltip: {
+    theme: "dark",
+    style: {
+      fontSize: "12px",
+    },
+  },
+  xaxis: {
+    categories: [
+      ["John", "Doe"],
+      ["Joe", "Smith"],
+      ["Jake", "Williams"],
+      "Amber",
+      ["Peter", "Brown"],
+      ["Mary", "Evans"],
+      ["David", "Wilson"],
+      ["Lily", "Roberts"],
+    ],
+    labels: {
+      style: {
+        colors: colors,
+        fontSize: "12px",
+      },
+    },
+  },
+  yaxis: {
+    labels: {
+      style: {
+        colors: "#ffffff",
+        fontSize: "12px",
+      },
+    },
+  },
+});
 </script>
 
 <template>
@@ -7,6 +92,14 @@ import MainLayout from "@/shared/layouts/MainLayout.vue";
     <div>
       <h1>Dashboard</h1>
       <p>Welcome to the dashboard!</p>
+      <div>
+        <apexchart
+          width="500"
+          type="bar"
+          :options="chartOptions"
+          :series="series"
+        ></apexchart>
+      </div>
       <router-link to="/login">Go to Login</router-link>
     </div>
   </MainLayout>
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,11 @@
 import "normalize.css";
 import { createApp } from "vue";
+import VueApexCharts from "vue3-apexcharts";
 import App from "./App.vue";
 import router from "./router";
 import "./scss/layout.scss";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+app.use(router);
+app.component("apexchart", VueApexCharts);
+app.mount("#app");
